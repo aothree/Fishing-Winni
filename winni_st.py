c9f611b48e8dcafbdaea25bc5ed5cf6a769cf80f
andy orfalea <aorfalea@gmail.com>
2022-06-11 12:42:33 -0700

    getting into streamlit app

---
+++ winni_st.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+st.title("Helping Bill Catch Fish on Lake Winnepausakee")
+
+
